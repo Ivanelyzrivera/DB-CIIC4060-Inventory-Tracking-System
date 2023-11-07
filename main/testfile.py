68ff3075a1357a19f6b292bb9271e9ab8ba03ef0
misaelmercado1 <78518246+misaelmercado1@users.noreply.github.com>
2023-11-07 12:56:20 -0400

    Some test files added for testing purposes

--- main/testfile.py
+++ main/testfile.py
@@ -1,1 +1,19 @@
-print("hello World")
+from flask import Flask, jsonify,request
+from flask_cors import CORS,cross_origin
+from handler import inventory_handler
+
+app = Flask(__name__)
+
+#apply Cors
+CORS(app)
+
+@app.route('/')
+def greeting():
+    return 'Hello, this is a test file asdasd'
+
+@app.route('/DB_Project/getAllparts')
+def getAllParts():
+    return PartHandler().getAllParts()
+
+if __name__ == '__main__':
+    app.run(debug=True)
