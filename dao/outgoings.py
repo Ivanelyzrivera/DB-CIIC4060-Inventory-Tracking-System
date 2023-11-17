094c794255846760822eb9eb4987fe7180b809d8
CarlosNg64 <carlos.iglesias@upr.edu>
2023-11-17 00:15:16 -0400

    Outgoing CRUD implemented

--- dao/outgoings.py
+++ dao/outgoings.py
@@ -23,3 +23,40 @@ def getAllOutgoings(self):
         print("An error occurred: ", e)
     finally:
         cursor.close()
+
+    def getoutgoingbyID(self,oid):
+        cursor = self.conn.cursor()
+        query = "SELECT O_ID, O_SellPrice, O_Customer, O_Destination, T_ID FROM Outgoing where o_id=%s"
+        try:
+            cursor.execute(query, (oid,))
+            result = cursor.fetchone()
+            return result
+        except Exception as e:
+            print("An error occurred: ", e)
+        finally:
+            cursor.close()
+
+    def insertOutgoing(self,sellprice,customer,destination,transactionID):
+        cursor = self.conn.cursor()
+        query = "insert into outgoing(O_SellPrice, O_Customer, O_Destination, T_ID) values (%s, %s, %s, %s) returning O_ID"
+        cursor.execute(query, (sellprice,customer,destination,transactionID,))
+        O_ID = cursor.fetchone()[0]
+        self.conn.commit()
+        return O_ID
+    
+    def deleteById(self, oid):
+        cursor = self.conn.cursor()
+        query = " delete FROM Outgoing where o_id =%s"
+        cursor.execute(query, (oid,))
+        count = cursor.rowcount
+        self.conn.commit()
+        return count
+    
+    def putById(self,oid,sellprice,customer,destination,transactionID):
+        cursor = self.conn.cursor()
+        query = "update outgoing set O_SellPrice = %s, O_Customer =%s, O_Destination = %s, T_ID = %s where o_id = %s;"
+        cursor.execute(query, (sellprice,customer,destination,transactionID, oid))
+        count = cursor.rowcount
+        self.conn.commit()
+        return count
+    
