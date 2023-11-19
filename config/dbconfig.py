48868018d33a9e35679b2f52999e4ff48b1a4d89
misaelmercado1 <78518246+misaelmercado1@users.noreply.github.com>
2023-11-19 18:56:32 -0400

    changes host and db to heroku

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,6 +1,6 @@
 
 pg_config = {
-    'user' : 'user3',
-    'password' :'user3',
-    'dbname' : 'postgres'
+    'user' : 'ec2-3-210-173-88.compute-1.amazonaws.com',
+    'password' :'83d0527775b3411c847bc2a9165fa09a7e227dab89478715ebdaf6cb1931c508',
+    'dbname' : 'd3tmtkrcp8aqj'
 }
