3efd87c21518810b34bf5765a8e2d3fe6c1c7d36
misaelmercado1 <78518246+misaelmercado1@users.noreply.github.com>
2023-11-19 19:06:11 -0400

    fixed dbconfig

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,6 +1,6 @@
 
 pg_config = {
-    'user' : 'ec2-3-210-173-88.compute-1.amazonaws.com',
+    'user' : 'zwemdsohihiqcl',
     'password' :'83d0527775b3411c847bc2a9165fa09a7e227dab89478715ebdaf6cb1931c508',
     'dbname' : 'd3tmtkrcp8aqj'
 }
