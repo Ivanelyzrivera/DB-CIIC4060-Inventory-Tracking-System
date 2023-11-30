92a0bf5efd71723fc23e741415e2c9967682a105
misaelmercado1 <78518246+misaelmercado1@users.noreply.github.com>
2023-11-30 14:04:31 -0400

    Updated db credentials

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -1,6 +1,6 @@
 
 pg_config = {
-    'user' : 'zwemdsohihiqcl',
-    'password' :'83d0527775b3411c847bc2a9165fa09a7e227dab89478715ebdaf6cb1931c508',
-    'dbname' : 'd3tmtkrcp8aqj'
+    'user' : 'ovyyzbvclcabgy',
+    'password' :'02a04b02be1473158c652e275c09b248131e9c431a32c187235adf1d75efb5cd',
+    'dbname' : 'ddi6u4bd9jt764'
 }
