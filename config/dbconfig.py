dd5987029491363d24ad94bfdae4473adabef147
misaelmercado1 <78518246+misaelmercado1@users.noreply.github.com>
2023-12-06 18:21:15 -0400

    fixed dbconfig again

--- config/dbconfig.py
+++ config/dbconfig.py
@@ -2,5 +2,5 @@
 pg_config = {
     'user' : 'ovyyzbvclcabgy',
     'password' :'02a04b02be1473158c652e275c09b248131e9c431a32c187235adf1d75efb5cd',
-    'dbname' : 'ovyyzbvclcabgy'
+    'dbname' : 'ddi6u4bd9jt764'
 }
